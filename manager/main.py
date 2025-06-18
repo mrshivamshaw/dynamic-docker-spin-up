bac33eff20caa039e9c4cf68bed77b067b7f1469
shivamshaw07 <shivamshaw9005@gmail.com>
2025-06-18 16:59:09 +0530

    updated stop container stop logic

--- manager/main.py
+++ manager/main.py
@@ -1,17 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 import docker
 import random
+import uuid
 
 app = FastAPI()
 client = docker.from_env()
+container_map = {}  # key: run_id, value: container_id
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows only specified origins
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],  # Allows all HTTP methods (GET, POST, PUT, DELETE, etc.)
-    allow_headers=["*"],  # Allows all headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.post("/run")
@@ -34,10 +36,32 @@ def run_container():
             detach=True
         )
 
+        run_id = str(uuid.uuid4())
+        container_map[run_id] = container.id
+        print(container_map)
         return {
+            "run_id": run_id,
             "frontend_url": f"http://localhost:{frontend_port}",
-            "backend_url": f"http://localhost:{backend_port}/api/hello"
+            "backend_url": f"http://localhost:{backend_port}/api/hello",
         }
+
+    except Exception as e:
+        return {"error": str(e)}
+
+@app.post("/stop")
+async def stop_container(request: Request):
+    try:
+        data = await request.json()
+        run_id = data.get("run_id")
+        if not run_id or run_id not in container_map:
+            return {"error": "Invalid run_id"}
+
+        container_id = container_map[run_id]
+        container = client.containers.get(container_id)
+        container.kill()
+        del container_map[run_id]
+
+        return {"message": f"Container with run_id {run_id} stopped successfully"}
 
     except Exception as e:
         return {"error": str(e)}
